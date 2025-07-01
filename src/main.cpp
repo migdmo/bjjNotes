025f2e31b99fc3f98a1212c7aefb914ee25a1d03
Miguel Oliveira <Miguel Oliveira>
2025-07-01 10:52:48 -0300

    begining to experiment with file creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "Session.hpp"
-
+#include <fstream>
 int main(){
     Session session;
-    session.set_position("half guard pass");
-    
-    session.get_positions();
+    session.set_date("01/07");
+    session.set_rolls(2);
+
+    session.set_insights("today i defended pretty good against a black belt");
+    std::ofstream("sess.txt") << session.get_insights() << std::endl;
+  
     return 0;
 }
