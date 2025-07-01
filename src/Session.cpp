a842de41ce2f0950195d19672a66babd60c8c117
Miguel Oliveira <Miguel Oliveira>
2025-07-01 13:54:59 -0300

    implementation of constructor method

--- src/Session.cpp
+++ src/Session.cpp
@@ -2,39 +2,17 @@
 
 #include "Session.hpp"
 
-void Session::set_date(std::string session_date)
+Session::Session(std::string date, std::vector<std::string> positions, std::string insights, int rolls, int passes, int defences):
+    date(date),
+    positions(positions),
+    insights(insights),
+    rolls(rolls),
+    passes(passes),
+    defences(defences)
 {
-    date = session_date;
 }
 
-void Session::set_position(std::string session_position)
-{
-    positions.push_back(session_position);
-}
-
-void Session::set_description(std::string description)
-{
-    position_description = description;
-}
-
-void Session::set_insights(std::string session_insights)
-{
-    insights = session_insights;
-}
-void Session::set_rolls(int session_rolls)
-{
-    rolls = session_rolls;
-}
-void Session::set_passes(int session_passes)
-{
-    passes = session_passes;
-}
-void Session::set_defences(int session_defences)
-{
-    defences = session_defences;
-}
-
-void Session::get_positions()
+void Session::get_positions() const
 {
     for (int i = 0; i < positions.size(); i++)
     {
@@ -42,21 +20,21 @@ void Session::get_positions()
     }
 }
 
-std::string Session::get_date(){
+std::string Session::get_date() const {
     return date;
 }
-std::string Session::get_description(){
+std::string Session::get_description() const {
     return position_description;
 }
-std::string Session::get_insights(){
+std::string Session::get_insights() const {
     return insights;
 }
-int Session::get_rolls(){
+int Session::get_rolls() const {
     return rolls;
 }
-int Session::get_passes(){
+int Session::get_passes() const {
     return passes;
 }
-int Session::get_defences(){
+int Session::get_defences() const{
     return defences;
 }
